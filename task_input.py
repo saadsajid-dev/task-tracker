86f785f1e91a807992f03601aff1598cb26bc6c9
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 11:49:00 +1000

    Week1Day3 - Task1&2 - Saad - Add comment header and welcome message

---
+++ task_input.py
@@ -0,0 +1,9 @@
+#Program: Task Tracker - Task Input
+#Authors: Saad, Emmanuel, Eddy
+#Description: Collects details from the user and displays formatted summary
+
+print ("=" * 40)
+print ("      Welcome to the Task Tracker!")
+print ("=" * 40)
+print ("Please enter your task details below. ")
+print ()
