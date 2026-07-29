0e7d7b71224240c0f32ea2e6105f97cbf1e6a90d
Eddy <ed12.gu12@gmail.com>
2026-07-29 11:53:03 +1000

    Task 3 and 4

---
+++ task_input.py
@@ -0,0 +1,11 @@
+task_name = input("Enter Task Name: ")
+task_prio = input("Enter Task Priority: (High/Medium/Low) ")
+task_time = input("Enter Estimated Time To Complete In Minutes: ")
+task_urgency = input("Is This Task Urgent: (Yes/No) ")
+
+print('')
+print('----------------Task Summary----------------')
+print('Task Name:', task_name)
+print('Task Priority:', task_prio)
+print('Estimated Time To Complete:', task_time, 'minutes')
+print('Task Urgency:', task_urgency)
