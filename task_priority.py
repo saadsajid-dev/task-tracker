97402269f1b583d62c4da89a29e50acf132b14b4
Eddy <ed12.gu12@gmail.com>
2026-07-30 12:14:54 +1000

    created priority loop and error handling

--- task_priority.py
+++ task_priority.py
@@ -11,13 +11,25 @@ print()
 while True:
     task_name = input("Enter Task Name (or type 'quit' to exit): ")
 
+    while len(task_name) <= 0:
+        task_name = input("Error. Please try another task name: ")
+
     if task_name.lower() == "quit":
         print()
         print("Session ended.Goodbye!")
         break
-
-    priority = input("Enter Task Priority (High/Medium/Low): ")
-
-    print()
-
-print("thanks, bye!")
+    while True:
+        priority = input("Enter Task Priority (High/Medium/Low): ").lower()
+        if priority == "high":
+            print("Urgent: handle this task first.")
+            break
+        elif priority == "medium":
+            print("Important: schedule this task soon.")
+            break
+        elif priority == "low":
+            print("Low priority: handle when time allows.")
+            break
+        else:
+            print("Priority not recognized. Please enter high, medium, or low.")
+    print("Task Added Successfully!")
+print()
