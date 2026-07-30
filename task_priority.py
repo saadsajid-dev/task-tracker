bccd9e2b3f88fb5790075cc336b7eec44d8c6a19
Saad Sajid <saadsajid47@icloud.com>
2026-07-30 11:58:11 +1000

    Update if quit loop and add priority input variable

--- task_priority.py
+++ task_priority.py
@@ -3,7 +3,7 @@
 #Description: Collects details from the user and displays formatted summary
 
 print("=" * 40)
-print("      Welcome to the Task Tracker!")
+print("      Welcome to the Task Tracker")
 print("=" * 40)
 print("Please enter your task details below.")
 print()
@@ -12,27 +12,11 @@ while True:
     task_name = input("Enter Task Name (or type 'quit' to exit): ")
 
     if task_name.lower() == "quit":
+        print()
+        print("Session ended.Goodbye!")
         break
 
-    task_prio = input("Enter Task Priority (High/Medium/Low): ")
-
-    # Input to integer
-    task_time = int(input("Enter Estimated Time To Complete In Minutes: "))
-
-    # Input to bool
-    task_urgency = input("Is This Task Urgent? (Yes/No): ").lower()
-    is_complete = (task_urgency == "yes")
-
-    print()
-    print("----------------Task Summary----------------")
-    print("Task Name:", task_name)
-    print("Task Priority:", task_prio)
-    print("Estimated Time To Complete:", task_time, "minutes")
-
-    if is_complete:
-        print("Task Urgency: Yes")
-    else:
-        print("Task Urgency: No")
+    priority = input("Enter Task Priority (High/Medium/Low): ")
 
     print()
 
