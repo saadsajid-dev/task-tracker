48767ceaba25029e528a1e4d163d059bc6949eb2
Saad Sajid <saadsajid47@icloud.com>
2026-07-31 11:01:42 +1000

    Created task_tracker.py file

---
+++ task_tracker.py
@@ -0,0 +1,3 @@
+#Program: Task Tracker - Main (Refactored task_priority.py with functions)
+#Authors: Saad, Emmanuel, Eddy
+#Description: Refactored version of task_priority.py that uses functions to collect details from the user and display a formatted summary.
