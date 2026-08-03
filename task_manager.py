8f4cf7047c5203074b566169004e87cc9ee6a2bb
Eddy <ed12.gu12@gmail.com>
2026-08-03 12:27:16 +1000

    set up program structure and add function

---
+++ task_manager.py
@@ -0,0 +1,36 @@
+tasks = []
+
+def add_task(name, priority, time):
+    tasks.append({'name':name, 'priority':priority, 'estimated_time':time})
+    print('Task added successfully')
+    print(tasks)
+    return
+
+def view_tasks():
+    pass
+
+def complete_task():
+    pass
+
+def delete_task():
+    pass
+
+def run_manager(name = 'Test', priority = 'low', time = 20):
+    while True:
+        user_input = input('Enter Command: ').lower()
+        if user_input == 'quit':
+            break
+        elif user_input == 'add':
+            add_task(name, priority, time)
+        elif user_input == 'view':
+            view_tasks()
+        elif user_input == 'complete':
+            complete_task()
+        elif user_input == 'delete':
+            delete_task()
+        else:
+            print('Error. Invalid Input')
+
+    print('Program Closing')
+
+run_manager()
